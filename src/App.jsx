79049b9767ccabb21a289c72ec8b94cefd030527
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-04-26 00:50:39 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './component/header'
+import React from 'react'
+import Notepage from './Modules/notes/Pages/Notepage';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Header/>
-      
-    </>
-  )
+   <div>
+    <Notepage/>
+   </div>
+  );
 }
 
-export default App
+export default App;
+
---
+++ src/Modules/notes/Pages/Notepage.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import Add from '../components/Add'
+import List from '../components/List'
+import Header from '../../../Shared/Component/Header'
+
+const  Notepage=()=> {
+  return (
+  <div>
+    <Header/>
+  <Add/>
+  <List/>
+  </div>
+  );
+}
+export default Notepage;
